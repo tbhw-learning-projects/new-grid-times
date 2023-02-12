35014907aeea94ba348d3d5f570e47d50481e1d9
Tanner B. Hess Webber <tb@hesswebber.com>
2023-02-12 15:44:17 +0000

    layout(header): adds desktop layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
+import Spacer from "../Spacer";
 
 const Header = () => {
   return (
@@ -29,7 +30,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <LargeScreenActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </LargeScreenActionGroup>
         <Logo />
+        <LargeScreenActionGroup direction="row-reverse" align="end">
+          <MemberLayout>
+            <Button>Subscribe</Button>
+            <MemberLink href="/">Already a member?</MemberLink>
+          </MemberLayout>
+        </LargeScreenActionGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +54,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +78,39 @@ const ActionGroup = styled.div`
   }
 `;
 
+const LargeScreenActionGroup = styled(ActionGroup)`
+  display: none;
+  flex-direction: ${({ direction }) => direction || "row"};
+  align-items: ${({ align }) => align || "center"};
+  flex: 1;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: flex;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
-  align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media (${QUERIES.laptopAndUp}) {
+    justify-content: space-between;
+  }
+`;
+
+const MemberLayout = styled.div``;
+
+const MemberLink = styled.a`
+  display: block;
+  text-align: center;
+  margin-top: 8px;
+  font-family: "Crimson Text";
+  font-style: italic;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
 `;
 
 export default Header;
